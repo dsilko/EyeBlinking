8e9ae9bb7f36fb530441631f674c19689b592f67
Denis <silkodenis@MacBook-Pro-Denis.local>
2015-12-16 16:51:40 +0200

    XCode project

---
+++ EyeBlinking/EyeBlinking/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EyeBlinking
+//
+//  Created by Denis on 16.12.15.
+//  Copyright © 2015 axondevgroup. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
